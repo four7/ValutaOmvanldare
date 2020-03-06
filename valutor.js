bba45d6766def7c2c164466d28d070c8ae4ac543
50964106 <i.pellarp@gmail.com>
2020-03-06 22:45:59 +0100

    cache attempt

--- valutor.js
+++ valutor.js
@@ -4,12 +4,17 @@ const toCurrency = document.getElementById('toCurrency');
 const toAmount = document.getElementById('toAmount');
 const diffRates = document.getElementById('rate');
 const convert = document.getElementById('convert');
+const cacheTime = 10;
+
 
 fromCurrency.addEventListener('change', getData);
 fromAmount.addEventListener('input', getData);
 toCurrency.addEventListener('change', getData);
 toAmount.addEventListener('input', getData);
  
+
+
+
 convert.addEventListener('click', () => {
 	const temp = fromCurrency.value;
 	fromCurrency.value = toCurrency.value;
@@ -22,14 +27,16 @@ async function getData() {
     const from_Currency = fromCurrency.value;
     const to_Currency = toCurrency.value;
     
-    await fetch(`https://api.exchangeratesapi.io/latest?base=${from_Currency}&symbols=${to_Currency}`)
+    await fetch(`https://api.exchangeratesapi.io/latest?base=${from_Currency}&symbols=${to_Currency}`,{cache: "no-cache"})
     .then(response => response.json())
     .then(res => {
         const rate = res.rates[to_Currency];
 		diffRates.innerText = `1 ${from_Currency} = ${rate} ${to_Currency}`
 		toAmount.value = (fromAmount.value * rate).toFixed(2);
     });
+    console.log(cache);
 }
 getData();
  
+
     
